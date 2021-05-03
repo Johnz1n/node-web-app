0b5e2a9d16328423481f0f94d7d304e2f5ac3f20
João Pedro Borça <joaopedroborca@gmail.com>
2021-05-03 15:06:35 -0300

    feat: github actions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - Test deploy without flux');
+  res.send('Hello World - Test deploy with github actions');
 });
 
 app.listen(PORT, HOST);
