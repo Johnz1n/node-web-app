dccf48aa549ff04513f8611b13dc3c3e2f14e517
João Pedro Borça <joaopedroborca@gmail.com>
2021-05-03 10:01:57 -0300

    test deploy with tag

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - Test deploy fluxctl :D');
+  res.send('Hello World - Test deploy fluxctl with tag');
 });
 
 app.listen(PORT, HOST);
