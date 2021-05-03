57625128fe17a04112db8316a4ed165784abe9c9
João Pedro Borça <joaopedroborca@gmail.com>
2021-05-03 13:43:18 -0300

    test deploy without flux

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - Test deploy fluxctl image pull policy');
+  res.send('Hello World - Test deploy without flux');
 });
 
 app.listen(PORT, HOST);
